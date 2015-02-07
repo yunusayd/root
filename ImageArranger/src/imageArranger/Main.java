84e8ac0f490258e8cef21de46d26c93321eed376
yunusayd <yunusayd@gmail.com>
2015-02-08 00:39:45 +0200

    deneme

--- ImageArranger/src/imageArranger/Main.java
+++ ImageArranger/src/imageArranger/Main.java
@@ -1,13 +1,37 @@
 package imageArranger;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
+	private JTextField textField;
 	public Main() {
+		getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		getContentPane().add(lblNewLabel, "2, 2, right, default");
+		
+		textField = new JTextField();
+		getContentPane().add(textField, "4, 2, fill, default");
+		textField.setColumns(10);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Main().setVisible(true);
 
 	}
 

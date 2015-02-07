84c99628c473bd2bfba7624633d3a603a1b1524e
yunusayd <yunusayd@gmail.com>
2015-02-08 00:34:32 +0200

    hadi bakalım

---
+++ ImageArranger/src/imageArranger/Main.java
@@ -0,0 +1,14 @@
+package imageArranger;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	public Main() {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
